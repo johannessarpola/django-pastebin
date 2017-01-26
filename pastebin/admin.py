af3a854d02045488eba268fac890b580ec3489a7
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-01-26 22:54:59 +0200

    models

--- pastebin/admin.py
+++ pastebin/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Text, User
+
+admin.site.register(Text)
+admin.site.register(User)
--- pastebin/models.py
+++ pastebin/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=120)
+    display_name = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+
+class Text(models.Model):
+    text_field = models.TextField()
+    date = models.DateField('date pasted')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
