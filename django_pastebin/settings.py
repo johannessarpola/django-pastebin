a21ef43f0108e71138dccb74539e74b04a049d79
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-01-28 17:28:54 +0200

    settings

--- django_pastebin/settings.py
+++ django_pastebin/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'pastebin.apps.PastebinConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
