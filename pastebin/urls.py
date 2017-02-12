d4dd75297321b1f92cf9bc56ac2b3d22668f916b
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-02-12 16:50:28 +0200

    modified navigation and added forgot pw link to login

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<paste_hash>\d+)/$', views.detail, name='detail'),
-    url(r'^invalid/', views.invalid_hash, name='invalid_hash'),
+    url(r'^invalid', views.invalid_hash, name='invalid_hash'),
     url(r'^new', views.new_paste, name='new'),
     url(r'^login', views.login, name='login'),
-    url(r'^logout/', views.logout, name='logout'),
+    url(r'^logout', views.logout, name='logout'),
     url(r'^register', views.register_user, name='register'),
     url(r'^forgot', views.forgot_password, name='forgot'),
     url(r'^me', views.profile, name='profile'),
