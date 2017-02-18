c2abf4404a14a106a093f30366c8c061353fa19b
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-02-18 13:39:37 +0200

    Fixed the trading urls leading to wrong ‘higher’ up urls

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -5,14 +5,15 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^invalid', views.invalid_hash, name='invalid_hash'),
-    url(r'^new', views.new_paste, name='new'),
-    url(r'^login', views.login_view, name='login'),
-    url(r'^logout', views.logout, name='logout'),
-    url(r'^register', views.register_user, name='register'),
-    url(r'^forgot', views.forgot_password, name='forgot'),
-    url(r'^me', views.me, name='profile'),
-    url(r'^about', views.about, name='about'),
+    url(r'^invalid$', views.invalid_hash, name='invalid_hash'),
+    url(r'^new$', views.new_paste, name='new'),
+    url(r'^login$', views.login_view, name='login'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^register$', views.register_user, name='register'),
+    url(r'^forgot$', views.forgot_password, name='forgot'),
+    url(r'^me$', views.me, name='me'),
+    url(r'^me/edit', views.edit_profile, name='edit_profile'),
+    url(r'^about$', views.about, name='about'),
     url(r'^view/(?P<paste_hash>\d+)', views.view_paste, name='view'),
 ]
 # Api endpoints
