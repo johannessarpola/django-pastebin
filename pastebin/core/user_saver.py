d54b5a36476d17d376253a0a27786aca1876aabf
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-02-18 13:44:18 +0200

    Added saving for extra info during user saving as well

--- pastebin/core/user_saver.py
+++ pastebin/core/user_saver.py
@@ -9,8 +9,16 @@ class UserSaver:
         from pastebin.forms.user_forms import RegistrationForm
         registration_form = RegistrationForm(request.POST)
         if registration_form is not None:
-            entity = registration_form.save(commit=True) # TODO Should probably log if there's problems
-            return entity
+            user = registration_form.save(commit=True)
+            self.save_extra_info(user)
+            return user
         else:
             self.logger.warning("Couldn't create user from form")
             return None
+
+    def save_extra_info(self, user):
+        from pastebin.models import UserExtraInfo
+        e_info = UserExtraInfo()
+        e_info.user = user
+        e_info.save()
+        e_info.paste_count = 0
